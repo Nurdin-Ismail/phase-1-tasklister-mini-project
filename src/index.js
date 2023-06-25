c3d7f50f47b4cbd3d667bccf7d5d00f5acc7dc04
Nurdin <nurdin.ismail@student.moringaschool.com>
2023-06-26 01:25:40 +0300

    completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    handleToDo(e.target['new-task-description'].value);
+    console.log(e.target['new-task-description'].value)
+    form.reset();
+  });
+  
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.getElementById('tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
